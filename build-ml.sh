a1d608b7e9735eda37a5e5c4a947b30cef934c6c
avaer <a@modules.io>
2018-11-11 04:38:46 -0500

    Add libx264 to build-ml.sh

--- build-ml.sh
+++ build-ml.sh
@@ -23,6 +23,16 @@ CC="$MLSDK/tools/toolchains/bin/aarch64-linux-android-gcc" make clean
 CC="$MLSDK/tools/toolchains/bin/aarch64-linux-android-gcc" make -j4
 popd
 
+pushd ./deps/x264
+CC="$MLSDK/tools/toolchains/bin/aarch64-linux-android-gcc" ./configure --host=aarch64-linux-android --enable-static
+find . -type f -iname '*.Plo' -exec sed -i 's/C:\\/\/mnt\/c\//g' "{}" +;
+find . -type f -iname '*.Plo' -exec sed -i 's/\([^ ]\)\\/\1\//g' "{}" +;
+find . -type f -iname '*.Po' -exec sed -i 's/C:\\/\/mnt\/c\//g' "{}" +;
+find . -type f -iname '*.Po' -exec sed -i 's/\([^ ]\)\\/\1\//g' "{}" +;
+CC="$MLSDK/tools/toolchains/bin/aarch64-linux-android-gcc" make clean
+CC="$MLSDK/tools/toolchains/bin/aarch64-linux-android-gcc" make -j4
+popd
+
 # https://chromium.googlesource.com/chromium/third_party/ffmpeg/+/master/chromium/config/Chrome/linux/x64/config.h
 ./configure --disable-everything --disable-all --disable-doc --disable-htmlpages --disable-manpages --disable-podpages --disable-txtpages --disable-static --enable-avcodec --enable-avformat --enable-avutil --enable-fft --enable-rdft --enable-static --enable-libopus --disable-debug --disable-bzlib --disable-error-resilience --disable-iconv --disable-lzo --disable-network --disable-schannel --disable-sdl2 --disable-symver --disable-xlib --disable-zlib --disable-securetransport --disable-faan --disable-alsa --disable-autodetect --enable-decoder='vorbis,libopus,flac' --enable-decoder='pcm_u8,pcm_s16le,pcm_s24le,pcm_s32le,pcm_f32le,mp3' --enable-decoder='pcm_s16be,pcm_s24be,pcm_mulaw,pcm_alaw' --enable-demuxer='ogg,matroska,wav,flac,mp3,mov' --enable-parser='opus,vorbis,flac,mpegaudio' \
 --disable-linux-perf --x86asmexe=yasm --enable-small --enable-cross-compile \
